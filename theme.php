54fcf7abaa6ac90c470b2387ff7336780ef5b209
Kamaro Lambert <kamaroly@gmail.com>
2014-11-26 16:24:10 +0200

    first commit

---
+++ theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Interactive2 extends Theme {
+
+    public $name = 'Interactive2';
+    public $author = 'Kamaro Lambert';
+    public $author_website = 'http://www.kamaroly.com/';
+    public $website = 'http://www.interactivemedia-ltd.com/';
+    public $description = 'IM version 2.';
+    public $version = '1.0.0';
+
+}
+
+/* End of file theme.php */
